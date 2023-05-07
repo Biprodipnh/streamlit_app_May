41d7f05db461e0393b619142c33508f8c60ab312
Biprodip Sadhukhan <68503341+Biprodipnh@users.noreply.github.com>
2023-05-07 16:42:12 +0530

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -47,7 +47,7 @@ fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 add_my_fruit = streamlit.text_input("What fruit would you like to add?")
-#streamlit.stop()
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
