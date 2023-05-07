b3c56fa87b08e93b34e26c7344ed133b69dd3074
Biprodip Sadhukhan <68503341+Biprodipnh@users.noreply.github.com>
2023-05-07 14:22:29 +0530

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -34,7 +34,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 
 
 # take the json version of response and normalize it 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 # output as a table
 streamlit.dataframe(fruityvice_normalized)
 
