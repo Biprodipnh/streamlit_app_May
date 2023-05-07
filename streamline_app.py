b32797fdd66c44b94859de7b11d6f82a594bafeb
Biprodip Sadhukhan <68503341+Biprodipnh@users.noreply.github.com>
2023-05-07 17:04:35 +0530

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -59,19 +59,23 @@ streamlit.write('The user entered ', fruit_choice)
 # output as a table
 
 add_my_fruit = streamlit.text_input("What fruit would you like to add?")
-streamlit.stop()
+#streamlit.stop()
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
 
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
-my_cur.execute("insert into fruit_load_list values('from streamlit')")
+#Snowflake-related functions
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("select * from fruit_load_list")
+    return my_cur.fetchall()
+
+#Add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
+
+
+#my_cur.execute("insert into fruit_load_list values('from streamlit')")
 
 
